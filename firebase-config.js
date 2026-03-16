dc348554a8aa273724403e5b69eb8621b795da3d
Rexe <barmonrohit216@gmail.com>
2026-03-16 18:01:18 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAhM6D-DBm-9-l9GcVuFwops8AgvIIXH-c",
+  authDomain: "rexe-data-system.firebaseapp.com",
+  databaseURL: "https://rexe-data-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "rexe-data-system",
+  storageBucket: "rexe-data-system.firebasestorage.app",
+  messagingSenderId: "98130612961",
+  appId: "1:98130612961:web:b3621c39341a7452a6dda0"
+};
